7e78a5931878d3d1497e5651cd8d0c6fe6b0a357
Bucks1bunny <46489335+Bucks1bunny@users.noreply.github.com>
2022-01-29 17:42:09 +0200

    Messenger

---
+++ Messenger/Models/Datalayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using Npgsql;
+
+namespace Messenger.Models
+{
+    public class DataLayer
+    {
+        NpgsqlConnection con = new NpgsqlConnection();
+        public DataLayer()
+        {
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["mycon"].ConnectionString;
+        }
+        public UserModel login(string email, string password)
+        {
+            UserModel user = new UserModel();
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            string sql = "select * from tbluser where email='" + email + "' and password='" + password + "'";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            foreach (DataRow row in dt.Rows)
+            {
+                user.userid = Convert.ToInt32(row["userid"].ToString());
+                user.email = row["email"].ToString();
+                user.password = row["password"].ToString();
+            }
+            return user;
+        }
+        public List<UserModel> getusers(int id)
+        {
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            List<UserModel> userlist = new List<UserModel>();
+            string sql = "select * from tbluser where userid<>" + id;
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            foreach (DataRow row in dt.Rows)
+            {
+                UserModel user = new UserModel();
+                user.userid = Convert.ToInt32(row["userid"].ToString());
+                user.email = row["email"].ToString();
+                user.password = row["password"].ToString();
+                userlist.Add(user);
+            }
+            return userlist;
+        }
+
+    }
+}
---
+++ Messenger/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Messenger.Models
+{
+    public class UserModel
+    {
+        public int userid { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
